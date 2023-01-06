903bf8b1cdb6a4cbe29f3b43df0693ee508e57a9
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2023-01-06 14:19:01 +0530

    Update Progessbar.jsx

--- src/Component/Progessbar.jsx
+++ src/Component/Progessbar.jsx
@@ -1,22 +1,50 @@
-import React from 'react';
-import { render } from 'react-dom';
-import CircularProgressbar from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import CircularProgress from 'react-native-circular-progress-indicator';
 
-const percentage = 90;
+export default function App() {
+  const [value, setValue] = useState(0);
 
-const App = () => (
-  <div >
-    <CircularProgressbar 
-      percentage={percentage}
-      text={`${percentage}%`}
-      styles={{
-        path: {
-          transform: 'rotate(90deg)',
-          transformOrigin: 'center center',
-        },
-      }}
-      counterClockwise
-    />
-  </div>
-);
+
+  return (
+    <View style={styles.container}>
+      <Text>Remember to subscribe to Code Palace!</Text>
+
+      <CircularProgress
+        radius={90}
+        value={85}
+        textColor='#222'
+        fontSize={20}
+        valueSuffix={'%'}
+        inActiveStrokeColor={'#2ecc71'}
+        inActiveStrokeOpacity={0.2}
+        inActiveStrokeWidth={6}
+        duration={3000}
+        onAnimationComplete={() => setValue(50)}
+      />
+
+      <CircularProgress
+        radius={100}
+        value={value}
+        textColor='#222'
+        fontSize={20}
+        valueSuffix={'%'}
+        activeStrokeColor={'tomato'}
+        inActiveStrokeOpacity={0.2}
+        duration={4000}
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
