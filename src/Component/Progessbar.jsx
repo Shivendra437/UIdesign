955c9b24f60212f2f68147d139e909c8800ac0e5
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2023-01-06 15:09:17 +0530

    footer

--- src/Component/Progessbar.jsx
+++ src/Component/Progessbar.jsx
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import CircularProgress from 'react-native-circular-progress-indicator';
 
-export default function App() {
+export default function Progessbar() {
   const [value, setValue] = useState(0);
 
 
   return (
     <View style={styles.container}>
-      <Text>Remember to subscribe to Code Palace!</Text>
+      {/* <Text>Remember to subscribe to Code Palace!</Text> */}
 
       <CircularProgress
         radius={90}
@@ -23,18 +23,6 @@ export default function App() {
         duration={3000}
         onAnimationComplete={() => setValue(50)}
       />
-
-      <CircularProgress
-        radius={100}
-        value={value}
-        textColor='#222'
-        fontSize={20}
-        valueSuffix={'%'}
-        activeStrokeColor={'tomato'}
-        inActiveStrokeOpacity={0.2}
-        duration={4000}
-      />
-
       <StatusBar style="auto" />
     </View>
   );
